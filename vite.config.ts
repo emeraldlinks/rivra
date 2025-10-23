6f37ce03a477fa751f5bea7047384ead3f33d95b
emeraldlinks <josephchukwumachristopher@gmail.com>
2025-10-23 02:31:20 +0100

    added full stack capability to the tools using fastify

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import { ripple } from 'vite-plugin-ripple';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-	plugins: [ripple()],
-	build: {
-		target: 'esnext',
-		minify: false,
-		sourcemap: mode !== 'production',
-		lib: {
-			entry: 'src/index.ts',
-			formats: ['es'],
-			fileName: 'index',
-		},
-		rollupOptions: {
-			external: ['ripple', /^ripple\//],
-		},
-	},
-	root: path.resolve(__dirname),
+  plugins: [...(ripple() as PluginOption[])],
+  build: {
+    target: 'esnext',
+    minify: false,
+    sourcemap: mode !== 'production',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['ripple', /^ripple\//],
+    },
+  },
+  root: path.resolve(__dirname),
 }));
